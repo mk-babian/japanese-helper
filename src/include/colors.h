be42a430fb8544ecb21ece3b4020cba1cae3a4ad
mk-babian <arslanemirsaba@gmail.com>
2026-09-18 09:25:19 +0400

    refactor: move Fl_Color fields into StyleColors struct
    Replace global color constants with instance fields in StyleColors to support per-style color configuration.

--- src/include/colors.h
+++ src/include/colors.h
@@ -11,10 +11,16 @@ struct StyleColors{
     std::string background;
     std::string foreground;
     std::vector<std::string> colors;
+
+    Fl_Color bg_color;
+    Fl_Color fg_color;
+    Fl_Color accent_0;
+    Fl_Color accent_1;
+    Fl_Color accent_2;
 };
 
-const Fl_Color bg_color = fl_rgb_color(202, 202, 205);
-const Fl_Color accent_grey = fl_rgb_color(182, 182, 188);
-const Fl_Color accent_red = fl_rgb_color(178, 80, 70);
-const Fl_Color accent_blue = fl_rgb_color(0, 120, 215);
-const Fl_Color accent_green = fl_rgb_color(54, 192, 96);
+// const Fl_Color bg_color = fl_rgb_color(202, 202, 205);
+// const Fl_Color accent_grey = fl_rgb_color(182, 182, 188);
+// const Fl_Color accent_red = fl_rgb_color(178, 80, 70);
+// const Fl_Color accent_blue = fl_rgb_color(0, 120, 215);
+// const Fl_Color accent_green = fl_rgb_color(54, 192, 96);
